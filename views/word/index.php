bf91969cb2be05faa780757bf85b2e5dac2322ff
beerfleet <jvbiervliet@gmail.com>
2026-08-25 16:40:18 +0200

    Words vertaald naar Woorden

--- views/word/index.php
+++ views/word/index.php
@@ -10,7 +10,7 @@ use yii\grid\GridView;
 /** @var app\models\WordSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Words';
+$this->title = 'Woorden';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="word-index">
